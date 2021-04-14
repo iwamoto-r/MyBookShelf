e3275caf11b94a4fad0551754db38ba355de9df6
iwamoto <azu7uki@gmail.com>
2021-04-14 19:18:32 +0900

    add new file

---
+++ database.php
@@ -0,0 +1,8 @@
+<?php
+
+// 接続情報
+define('DB_HOST', 'localhost'); // データベースのホスト名又はIPアドレス
+define('DB_USER', 'root'); // MySQLのユーザ名
+define('DB_PASSWD', ''); // MySQLのパスワード
+define('DB_NAME', 'mybookshelf') // データベース名
+?>
